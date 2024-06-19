b68f4e8d62845a4d634b5097423659b9f5e3e746
VsevolodVechnosadov <vsevolod_04@mail.ru>
2024-06-19 12:00:08 +0300

    user_mapping added, user_repository modified

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,21 +1,53 @@
 from model.user import User
 
+from data.execution import execute_query
+from data.read_all_execution import execute_read_query
+
+from mapping.user_mapping import user_from_row
+
+
 
 class UserRepository:
 
-    async def create(self, user_id: int) -> User:
-        pass
-    
-    async def read_by_id(self, user_id: int):
-        pass
+    def create(self, user: User):
+        execute_query(
+            '''INSERT INTO users (id, state_id, is_admin, is_true_admin) VALUES (%s, %s, %s, %s)''',
+            (
+                user.id,
+                user.state_id,
+                user.is_admin,
+                user.is_true_admin,
+            )
+        )
+   
+    def read_by_id(self, user: User) -> User:
+        row = execute_read_query(
+            '''SELECT * FROM users WHERE id = %s''',
+            (user.id,)
+        )
+        return user_from_row(row)
 
-    async def change_state_id(self, state_id: int):
-        pass
+    def change_state_id(self, user: User):
+        execute_query(
+            '''UPDATE users SET state_id = %s WHERE id = %s''',
+            (
+                user.state_id,
+                user.id,
+            )
+        )
     
-    async def set_admin(self, user: User, is_admin: bool):
-        pass
+    def set_admin(self, user: User):
+        execute_query(
+            '''UPDATE users SET is_admin = %s WHERE id = %s''',
+            (
+                user.is_admin,
+                user.id,
+            )
+        )
 
-    async def is_admin_check(self, is_admin: bool, is_true_admin: bool):
-        pass
-
-    
+    def is_admin_check(self, user: User) -> bool:
+        row = execute_read_query(
+            '''SELECT is_admin FROM users WHERE id = %s''',
+            (user.id,)
+        )
+        return row[0]
