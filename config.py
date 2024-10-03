0173fd89f2d974004000a78ac50a010348a649e7
VsevolodVechnosadov <vsevolod_04@mail.ru>
2024-10-03 19:48:38 +0300

    Ты переменные среды-то на клин не ложи

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 USE_WEBHOOK = os.getenv('USE_WEBHOOK').lower() == 'true'
-WEBHOOK_HOST = os.getenv('RAILWAY_PRIVATE_DOMAIN')
-WEBHOOK_PORT = os.getenv('PORT', default=5000)
+WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
+WEBHOOK_PORT = os.getenv('WEBHOOK_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
