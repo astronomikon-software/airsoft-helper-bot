44dcbb08234a859cacf566dc209942263290bda4
VsevolodVechnosadov <vsevolod_04@mail.ru>
2024-12-17 20:38:43 +0300

    Простые решения

--- utils/button_util.py
+++ utils/button_util.py
@@ -6,3 +6,12 @@ def create_button(text: str, callback: str) -> types.InlineKeyboardButton:
         text=text,
         callback_data=callback
     )
+
+
+def create_multiselect_button(text: str, is_selected: bool, callback: str) -> types.InlineKeyboardButton:
+    if is_selected:
+        text += ' ✅'
+    return types.InlineKeyboardButton(
+        text=text,
+        callback_data=callback
+    )
